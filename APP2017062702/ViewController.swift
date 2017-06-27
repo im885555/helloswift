054c733a91c420a8862cf44ebb2db7cbf6c417c5
GeneChen <im885555@gmail.com>
2017-06-27 11:10:43 +0800

    Initial Commit

---
+++ APP2017062702/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APP2017062702
+//
+//  Created by GeneChen on 2017/6/27.
+//  Copyright © 2017年 GeneChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
